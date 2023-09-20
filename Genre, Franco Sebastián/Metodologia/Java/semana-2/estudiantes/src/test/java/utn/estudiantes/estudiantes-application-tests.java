51bd59a7d96a29ca2ad70c9e1cba3bf9861ae951
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-19 23:07:40 -0300

    Create estudiantes-application-tests.java

---
+++ Genre, Franco Sebastián/Metodologia/Java/semana-2/estudiantes/src/test/java/utn/estudiantes/estudiantes-application-tests.java
@@ -0,0 +1,12 @@
+package utn.estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
