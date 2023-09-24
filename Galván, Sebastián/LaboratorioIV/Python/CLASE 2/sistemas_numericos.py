9782344a37b5cfa07486665bb924b0dcdbbf172d
Sebastian Galvan <sebastiangalvan.ar@gmail.com>
2023-09-24 00:44:47 -0300

    Calase 2 Python

---
+++ Galván, Sebastián/LaboratorioIV/Python/CLASE 2/sistemas_numericos.py
@@ -0,0 +1,41 @@
+# Profundizando en los sistemas de numeracion
+
+#Sistemas decimal
+
+a = 10
+print(f'a en decimal: {a}')
+
+#Sistemas binario
+
+a = 0b1010
+print(f'a en binario: {a}')
+
+#Sistema Octal
+a = 0o12
+
+print(f'a en octal: {a}')
+
+#Sistema Hexadecimal
+a = 0xA
+print(f'a es hexadecimal: {a}')
+
+#Base decimal
+a = int('23', 10)
+print(f'a en decimal {a}')
+
+#Base binaria
+a = int('10111', 2)
+print(f'a en binario {a}')
+
+#Base octal
+a = int('27', 8)
+print(f'a en octal {a}')
+
+#Base hexadecimal
+a = int('17', 16)
+print(f'a en hexadecimal {a}')
+
+
+#Base 5
+a = int('43', 5)
+print(f'a en base 5 {a}')
