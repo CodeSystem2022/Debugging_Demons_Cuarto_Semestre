fecb78b4ce84ccec3ac896ba7cab56fcecb061ad
Sebastian Galvan <sebastiangalvan.ar@gmail.com>
2023-09-24 14:54:18 -0300

    Clases 4 y 5

---
+++ Galván, Sebastián/LaboratorioIV/Python/CLASE5/str_inmutable.py
@@ -0,0 +1,9 @@
+# help(str.capitalize)
+mensaje1 = 'hola mundo'
+mensaje2 = mensaje1.capitalize()
+
+print(f'Mensaje 1: {mensaje1}, id: {id(mensaje1)}')
+print(f'Mensaje 2: {mensaje2}, id: {id(mensaje2)}')
+
+mensaje1 = ' Adios'
+print(f'Mensaje 1: {mensaje1}, id: {id(mensaje1)}')
