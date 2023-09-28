3eeeeb71fd77f8216b181dc91470f47c6dd12e9d
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2023-09-27 21:38:58 -0300

    Semana 1 - Mario Cristian Sanchez

---
+++ Sánchez, Mario Cristian/LaboratorioIV/Javascript/Clase1/E-COMMERCE2023/client/js/products.js
@@ -0,0 +1,49 @@
+const productos = [
+    {
+        id: 1,
+        producName: "Notebook",
+        price: 500000,
+        quanty: 1,
+        img: "/client/media/notebook.png"
+    },
+
+    {
+        id: 2,
+        producName: "Teclado",
+        price: 6200,
+        quanty: 1,
+        img: "/client/media/teclado.png"
+    },
+
+    {
+        id: 3,
+        producName: "Smart TV ",
+        price: 650000,
+        quanty: 1,
+        img: "/client/media/tv.png"
+    },
+
+    {
+        id: 4,
+        producName: "Play Station 5",
+        price: 425000,
+        quanty: 1,
+        img: "/client/media/playStation5.png"
+    },
+
+    {
+        id: 5,
+        producName: "Mouse",
+        price: 2500,
+        quanty: 1,
+        img: "/client/media/mouse.png"
+    },
+
+    {
+        id: 6,
+        producName: "Tablet",
+        price: 88000,
+        quanty: 1,
+        img: "/client/media/tablet.png"
+    }
+]
---
+++ Sánchez, Mario Cristian/Metodologia/Java/Clase 1/Conexion01.sql
@@ -0,0 +1,5 @@
+-- Comenzamos con CRUD: create(insertar), read(leer), update(actualizar), delete(eliminar)
+-- Listar los estudiantes (read)
+SELECT * FROM estudiantes2022;
+-- Insertar estudiante
+INSERT INTO estudiantes2022 (nombre, apellido, telefono, email) VALUES ("Lautaro", "Sanchez", "26045600560","Lauti@mail.com");
