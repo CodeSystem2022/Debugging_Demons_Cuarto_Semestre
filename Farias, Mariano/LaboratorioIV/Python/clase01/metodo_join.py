1dc3c3959133de62444e6c46515ab05bfcacf4ca
Marianoleonardofarias <101066897+Marianoleonardofarias@users.noreply.github.com>
2023-10-19 21:04:36 -0300

    Clase 5

---
+++ Farias, Mariano/LaboratorioIV/Python/clase01/metodo_join.py
@@ -0,0 +1,12 @@
+
+help(str.join)
+
+tupla_str = ('Hola', 'alumnos', 'Tecnicatura','Universitaria')
+mensaje = ' '.join(tupla_str)
+print(f'Mensaje: {Mensajes}')
+
+lista cursos = ['java', 'Python','Angular','Spring']
+mensaje = ', '.join(lista_cursos)
+print(f'Mensaje: {mensaje}')
+
+cadena = 'HolaMundo'
