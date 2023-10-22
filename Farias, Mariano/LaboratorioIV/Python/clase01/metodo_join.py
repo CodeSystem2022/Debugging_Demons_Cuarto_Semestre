7782d236665251b19a42c6cfd19cbdeb9491467b
Marianoleonardofarias <101066897+Marianoleonardofarias@users.noreply.github.com>
2023-10-22 12:41:55 -0300

    Seguimos

--- Farias, Mariano/LaboratorioIV/Python/clase01/metodo_join.py
+++ Farias, Mariano/LaboratorioIV/Python/clase01/metodo_join.py
@@ -10,3 +10,12 @@ mensaje = ', '.join(lista_cursos)
 print(f'Mensaje: {mensaje}')
 
 cadena = 'HolaMundo'
+mensaje = '.'.join(cadena)
+print(f'Mensaje:{Mensaje}')
+diccionario = {'nombre':'Juan','Apellido':'Perez','Edad': '18'}
+llaves = '-'.join(diccionario.keys())
+valores = '-'.join(diccionario.values())
+print(f'Llaves: {llaves}, Type: {type(llaves)}')
+print(f'valores:{valores},Type:{type(valores)}')
+
+
