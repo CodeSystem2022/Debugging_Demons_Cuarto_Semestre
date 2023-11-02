cd4bfe1e5e106160da6217661c5a46962a2f1e1b
Braian <troncosobraianaxel@gmail.com>
2023-11-02 17:27:25 -0300

    [ADD] act codigo node

--- Troncoso, Braian Axel/Metodologia/Node/02-Fundamentos_node/async/callback.js
+++ Troncoso, Braian Axel/Metodologia/Node/02-Fundamentos_node/async/callback.js
@@ -1,7 +1,20 @@
-function soyAsincrona(){
-    console.log('Hola, Soy una funcion asincrona');
+function hola(nombre, miCallBack){
+    setTimeout(function (){
+        console.log('Hola, '+nombre);
+        miCallBack(nombre);
+    },1000);
 }
 
-console.log('Iniciando el proceso')
-soyAsincrona();
-console.log('Finalizando el proceso')
+function adios(nombre, otraCallback){
+    setTimeout(function(){
+        console.log('Adios '+ nombre);
+        otraCallback();
+    },1000)
+}
+
+console.log('Iniciando el proceso...');
+hola('Carlos',function(nombre){
+    adios(nombre,function(){
+        console.log('Finalizando el proceso...')
+    });
+});
