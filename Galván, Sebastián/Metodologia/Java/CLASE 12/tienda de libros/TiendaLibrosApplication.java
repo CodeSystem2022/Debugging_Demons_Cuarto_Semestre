da42d6ce15c124a718008f7c8d416489274b733f
Sebastian Galvan <sebastiangalvan.ar@gmail.com>
2023-11-10 02:32:33 -0300

    Implementacion servicio + Formulario + App principal

---
+++ Galván, Sebastián/Metodologia/Java/CLASE 12/tienda de libros/TiendaLibrosApplication.java
@@ -0,0 +1,29 @@
+package utn.tienda_libros;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+
+import utn.tienda_libros.vista.LibroFrom;
+
+import java.awt.*;
+
+@SpringBootApplication
+public class TiendaLibrosApplication {
+
+	public static void main(String[] args) {
+
+	ConfigurableApplicationContext contextoSpring =
+			new SpringApplicationBuilder(TiendaLibrosApplication.class)
+					.headless(false)
+					.web(WebApplicationType.NONE)
+					.run(args);
+	EventQueue.invokeLater(()->{ // Método lambda similar al flecha de js
+		LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+		libroFrom.setVisible(true);
+	});
+
+};
+};
