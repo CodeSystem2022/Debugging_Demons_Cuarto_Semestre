0ea264370bede4d17c00b553b4d896dfbaf1ea71
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2023-11-12 21:32:47 -0300

    [ADD] Clase 7 JavaScript - Java - Node

---
+++ Sánchez, Mario Cristian/LaboratorioIV/Javascript/Clase7/PERN-stack/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import morgan from "morgan";
+import tareasRoutes from "./router/tareas.routes.js"
+import authRoutes from "./router/auth.routes.js"
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+//Middlewares
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.json({ message: "Bienvenido a mi proyecto"}));
+
+app.use('/api',tareasRoutes);
+app.use('/api',authRoutes);
+
+
+//Manejando errores
+/*
+app.get('/test', (req, res) => {
+
+    throw new Error('Error custom');
+    res.send('test')
+});
+*/
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        status: "error",
+        message: err.message
+    });
+});
+
+export default app;
